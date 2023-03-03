e7c72a9322b9f53e0f85ec0462484a21c46263e0
nammflow <jeremyjwolff.tech@gmail.com>
2023-03-03 00:14:11 -0700

    Got split text working.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import { Application, Sprite } from 'pixi.js'
+import TaggedText from "pixi-tagged-text";
+import { WebfontLoaderPlugin } from "pixi-webfont-loader";
+import { Application, Loader, Sprite, Ticker } from "pixi.js";
+
+Loader.registerPlugin(WebfontLoaderPlugin)
+
+const loader = Loader.shared;
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -9,11 +15,49 @@ const app = new Application({
 	height: 480
 });
 
-const clampy: Sprite = Sprite.from("clampy.png");
+loader.add("Students", "../fonts/tss.ttf");
+loader.add("./clampy.png");
 
-clampy.anchor.set(0.5);
+loader.onComplete.add(() => start);
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+loader.load(start);
+function start () {
+	console.log("AESDF")
 
-app.stage.addChild(clampy);
+	const clampy: Sprite = Sprite.from("clampy.png");
+
+	clampy.anchor.set(0.5);
+
+	clampy.x = app.screen.width / 2;
+	clampy.y = app.screen.height / 2;
+
+	app.stage.addChild(clampy);
+	
+	const text1 = new TaggedText("Hello", {default: {
+			fontFamily: "Students",
+			fontSize: 120,
+			textTransform  : "capitalize",
+		}}, {debug: true, debugConsole: true, splitStyle: "characters"});
+	
+	let colors = ["#ff0000", "#00ff00", "#0000ff", "#ffff00", "#00ffff", "#ff00ff", "#ffffff", "#000000"];
+	let sizes = [240, 160, 120, 100, 80, 60, 40, 20];
+	
+	console.log(text1.textFields.forEach((t, i) => t.style.fill=colors[i]));
+	
+	// console.log(text1.tagStyles);
+	let timer = 0;
+	const numFramesPerColor = 400;
+	app.stage.addChild(text1);
+	Ticker.shared.add((delta) => {
+		timer += delta;
+		if (timer < 1000 / numFramesPerColor) return;
+		timer = 0;
+		colors = colors.slice(1).concat(colors[0]); // rotate colors
+		sizes = sizes.slice(1).concat(sizes[0]); // rotate sizes
+		text1.textFields.forEach((t, i) => {
+			t.style.fill = colors[i]
+			t.style.fontSize = sizes[i]
+		});
+		text1.setText("nowwa")
+	})
+}
