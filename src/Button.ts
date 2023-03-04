29f66b695cabe57dc37169d419f8308541440c39
JeremyWolff <jeremyjwolff.tech@gmail.com>
2023-03-03 18:04:04 -0700

    Added a few examples.

---
+++ src/Button.ts
@@ -0,0 +1,118 @@
+﻿import {NineSlicePlane, Texture, Text} from "pixi.js";
+
+export default class Button extends NineSlicePlane {
+    settings: {
+        width: number;
+        height: number;
+        x: number;
+        y: number;
+        fontSize: number;
+        label: string;
+        stroke: string;
+        strokeThickness: number;
+        onTap?: () => void;
+        onOver?: () => void;
+        onOut?: () => void;
+        onDown?: () => void;
+        onUp?: () => void;
+    };
+    label: Text;
+    
+    isOver = false;
+    isActive = false;
+    constructor(settings: any) {
+        const texture = Texture.from('button')
+        const notScalableArea = 20 // Indent from left, top, right and bottom sides in pixels
+        super(texture, notScalableArea, notScalableArea, notScalableArea, notScalableArea)
+
+        /** Contains settings for the button */
+        this.settings = {
+            // Default values
+            width: 200,
+            height: 100,
+            x: 300,
+            y: 300,
+
+            fontSize: 35,
+            label: 'Button',
+            stroke: '#336699',
+            strokeThickness: 4
+        }
+
+        // Main text on the button
+        this.label = new Text('')
+        this.label.anchor.set(0.5);
+        this.addChild(this.label)
+
+        // Update visual appearance
+        this.update(settings);
+        
+        this.interactive = true;
+        
+        this.buttonMode = true;
+        
+        this.on('pointertap', this.onTap);
+        this.on('pointerover', this.onOver);
+        this.on('pointerout', this.onOut);
+        this.on('pointerdown', this.onDown);
+        this.on('pointerup', this.onUp);
+        this.on('pointerupoutside', this.onUp);
+        
+    }
+    
+    onTap = () => {
+        if (this.settings.onTap) this.settings.onTap()
+    }
+
+    onOver = () => {
+        this.isOver = true
+        this.update()
+    }
+
+    onOut() {
+        this.isOver = false
+        this.update()
+    }
+
+    onDown() {
+        this.isActive = true
+        this.update()
+    }
+
+    onUp() {
+        this.isActive = false
+        this.update()
+    }
+
+    /** Updates the button's appearance after changing its settings */
+    update(settings?: any) {
+        // Creating new settings which include old ones and apply new ones over it
+        this.settings = {
+            ...this.settings, // including old settings
+            ...settings, // including new settings
+        }
+
+        this.label.text = this.settings.label
+        this.label.style = {
+            fontSize: this.settings.fontSize + 'px',
+            fill: '#ffffff',
+            stroke: this.settings.stroke,
+            strokeThickness: this.settings.strokeThickness,
+        }
+
+        this.onResize()
+    }
+
+    /** Changes sizes and positions each time when the button updates */
+    onResize() {
+        this.width = this.settings.width
+        this.height = this.settings.height
+        this.x = this.settings.x;
+        this.y = this.settings.y;
+
+        this.label.x = this.width * 0.5
+        this.label.y = this.height * 0.5
+
+        this.pivot.set(this.width * 0.5, this.height * 0.5)
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import TaggedText from "pixi-tagged-text";
 import { WebfontLoaderPlugin } from "pixi-webfont-loader";
 import {Application, Loader, Ticker, utils} from "pixi.js";
 import {hsl2rgb} from "./hue2rgb";
+import Button from "./Button";
 
 Loader.registerPlugin(WebfontLoaderPlugin)
 
@@ -12,41 +13,89 @@ const app = new Application({
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: 1024,
+	height: 768
 });
 
-loader.add("Students", "https://cdn.rhon.us/fonts/sweater_school_rg.otf");
+loader.add("Sweater", "https://cdn.rhon.us/fonts/sweater_school_rg.otf");
 loader.add("./clampy.png");
+loader.add("button", "./button.png");
 
 loader.onComplete.add(() => start);
 
 loader.load(start);
+
+const words = "The quick brown fox jumped over the lazy dog."
+
+let rainbowText =  new TaggedText("rainbow\n\n" + words, {default: {
+		fontFamily: "Sweater",
+		fontSize: 64
+	}}, {});
+
+let characterSplit = new TaggedText("character split\n\n" + words, {default: {
+		fontFamily: "Sweater",
+		fontSize: 64,
+	}}, {splitStyle: "characters"});
+
+let noSplit = new TaggedText("no split\n\n" + words, {default: {
+		fontFamily: "Sweater",
+		fontSize: 64,
+	}});
+
+let wordSplit = new TaggedText("word split\n\n" + words, {default: {
+		fontFamily: "Sweater",
+		fontSize: 64,
+	}}, {splitStyle: "words"});
+
+let tagExample = new TaggedText("We can also use custom tags for styling. Default and <smol>smol</smol> and <yuge>BIG</yuge>", {
+	default: {
+		fontFamily: "Arial",
+		fontSize: 64,
+		align: 'center'
+	},
+	smol: {
+		fontSize: 32,
+	},
+	yuge: {
+		fontSize: 128,
+	}});
+
+let things = [rainbowText, characterSplit, noSplit, wordSplit, tagExample];
+
+let currentThing = 0;
 function start () {
 	
-	const text1 = new TaggedText("Wow doesn't this look pretty?", {default: {
-			fontFamily: "Students",
-			fontSize: 80,
-		}}, {debugConsole: true, splitStyle: "characters"});
+	const button = new Button({label: "Click me!", x: 300, y: 600, width: 200, height: 100, onTap: nextThing});
+	addRainbowText();
+	makeTextClickable(characterSplit);
+	makeTextClickable(noSplit);
+	makeTextClickable(wordSplit);
 	
-	// text1.interactive = true;
-	
-	text1.on("pointerdown", () => {
-		window.alert("CLICKED " + text1.text);
+	app.stage.addChild(button);
+}
+
+const nextThing = () => {
+	app.stage.removeChild(things[currentThing]);
+	currentThing = (currentThing + 1) % things.length;
+	app.stage.addChild(things[currentThing]);
+}
+
+function addRainbowText () {
+
+	rainbowText.interactive = true;
+
+	rainbowText.on("pointerdown", () => {
+		window.alert("CLICKED " + rainbowText.text);
 	});
-	
-	text1.textFields.forEach((t) => {
+
+	rainbowText.textFields.forEach((t) => {
 		t.interactive = true;
 		t.on("click", () => {
 			t.style.fill = "#ff0000";
 		})
 	});
-	
-	app.stage.addChild(text1);
-	
+
 	// Create array of 50 unique colors in descending order from the rainbow
-	// let colors = ["#ff0000", "#ff7f00", "#ffff00", "#00ff00", "#0000ff", "#4b0082", "#8b00ff", "#ff0000", "#ff7f00", "#ffff00", "#00ff00", "#0000ff", "#4b0082", "#8b00ff", "#ff0000", "#ff7f00", "#ffff00", "#00ff00", "#0000ff", "#4b0082", "#8b00ff", "#ff0000", "#ff7f00", "#ffff00", "#00ff00", "#0000ff", "#4b0082", "#8b00ff", ]
-	// @ts-ignore
 	let colors2 = Array.from({length: 50}, (_, i) => {
 		const hue = i / 50 * 360;
 		const rgb = hsl2rgb(hue / 360, 1, 0.5);
@@ -60,8 +109,19 @@ function start () {
 		timer = 0;
 		colors2 = colors2.slice(1).concat(colors2[0]); // rotate colors
 		// sizes = sizes.slice(1).concat(sizes[0]); // rotate sizes
-		text1.textFields.forEach((t, i) => {
+		rainbowText.textFields.forEach((t, i) => {
 			t.style.fill = colors2[i]
 		});
 	})
+	
+}
+
+function makeTextClickable (text: TaggedText) {
+	text.interactive = true;
+	text.textFields.forEach((t) => {
+		t.interactive = true;
+		t.on("click", () => {
+			t.style.fill = "#ff0000";
+		})
+	});
 }
